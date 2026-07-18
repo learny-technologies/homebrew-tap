0591031d4e839829cd4c5ad8d950b0083998623e
Vladyslav Reva <averdalv@gmail.com>
2026-07-18 19:50:43 +0300

    Update controlpctl to 0.2.5

--- Formula/controlpctl.rb
+++ Formula/controlpctl.rb
@@ -2,12 +2,12 @@ class Controlpctl < Formula
   desc "Learny Technologies Control Plane command-line client"
   homepage "https://github.com/learny-technologies/control-plane-workspace"
   url "https://github.com/learny-technologies/homebrew-tap/archive/refs/tags/controlpctl-bootstrap-0.2.0.tar.gz"
-  version "0.2.4"
+  version "0.2.5"
   sha256 "d520048952b9391f4c60968d8069f39ae7fa64b17ed3773cfd8cddced2dc7829"
   license "Proprietary"
 
   CONTROL_PLANE_REPOSITORY = "learny-technologies/control-plane-workspace".freeze
-  RELEASE_SHA256 = "ebd00a7afa18fab6354fa6ce529817fd2afa261253da7f1780e6474da43687a6".freeze
+  RELEASE_SHA256 = "a15f5958b52a12eeeebe75f65befe85cd11b8769c0b1dd9b0ed450c13b0a6064".freeze
 
   depends_on "gh"
   depends_on "python@3.12"
